6b0ffbc9db7b5d84151e3c264a7119eccab71693
zhuleiyue <zhuleiyue@gmail.com>
2016-01-26 15:10:29 +0800

    添加LoadMoreRecyclerView类

--- app/src/main/java/com/zly/loadmorerecyclerview/SampleActivity.java
+++ app/src/main/java/com/zly/loadmorerecyclerview/SampleActivity.java
@@ -3,11 +3,11 @@ package com.zly.loadmorerecyclerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sample);
     }
 }
