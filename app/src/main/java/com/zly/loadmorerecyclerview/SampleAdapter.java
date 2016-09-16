f43f4c55962d8f645ad42d97ac9958b2698998e7
zhuleiyue <zhuleiyue@gmail.com>
2016-09-16 13:26:11 +0800

    add lib

--- app/src/main/java/com/zly/loadmorerecyclerview/SampleAdapter.java
+++ app/src/main/java/com/zly/loadmorerecyclerview/SampleAdapter.java
@@ -1,79 +1,38 @@
 package com.zly.loadmorerecyclerview;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by zhuleiyue on 16/1/26.
  */
-public class SampleAdapter extends LoadMoreRecyclerView.LoadMoreAdapter {
-    private LayoutInflater mLayoutInflater;
+public class SampleAdapter extends RecyclerView.Adapter<SampleAdapter.ItemViewHolder> {
     private ArrayList<String> mData;
 
-    public SampleAdapter(Context context, ArrayList<String> data){
-        mLayoutInflater = LayoutInflater.from(context);
+    public SampleAdapter(ArrayList<String> data) {
         mData = data;
     }
 
     @Override
-    public int getCount() {
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_sample, parent, false));
+    }
+
+    @Override
+    public int getItemCount() {
         return mData == null ? 0 : mData.size();
     }
 
     @Override
-    public int getViewType(int position) {
-        return 0;
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateItemView(ViewGroup parent, int viewType) {
-        return new ItemViewHolder(mLayoutInflater.inflate(R.layout.list_item_sample, parent, false));
-    }
-
-    @Override
-    public void onBindItemView(RecyclerView.ViewHolder holder, int position) {
-        ((ItemViewHolder)holder).tvItem.setText(mData.get(position));
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateFooterViewHolder(ViewGroup parent) {
-        return new FooterViewHolder(mLayoutInflater.inflate(R.layout.footer_view_sample, parent, false));
-    }
-
-    @Override
-    protected void bindFooterItem(RecyclerView.ViewHolder holder) {
-        FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
-        switch (mLoadMoreStatus) {
-            case STATUS_LOADING:
-                holder.itemView.setVisibility(View.VISIBLE);
-                footerViewHolder.pb.setVisibility(View.VISIBLE);
-                footerViewHolder.tv.setText("正在加载更多...");
-                break;
-            case STATUS_EMPTY:
-                holder.itemView.setVisibility(View.VISIBLE);
-                footerViewHolder.pb.setVisibility(View.GONE);
-                footerViewHolder.tv.setText("没有更多了");
-                holder.itemView.setOnClickListener(null);
-                break;
-            case STATUS_ERROR:
-                holder.itemView.setVisibility(View.VISIBLE);
-                footerViewHolder.pb.setVisibility(View.GONE);
-                footerViewHolder.tv.setText("加载出错，点击重试");
-                holder.itemView.setOnClickListener(mListener);
-                break;
-            case STATUS_PREPARE:
-                holder.itemView.setVisibility(View.INVISIBLE);
-                break;
-        }
-
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.tvItem.setText(mData.get(position));
+        holder.itemView.setPadding(0, 0, 0, 100 + new Random().nextInt(100));
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
@@ -84,16 +43,4 @@ public class SampleAdapter extends LoadMoreRecyclerView.LoadMoreAdapter {
             tvItem = (TextView) itemView.findViewById(R.id.tv_item);
         }
     }
-
-    static class FooterViewHolder extends RecyclerView.ViewHolder {
-        ProgressBar pb;
-        TextView tv;
-
-        public FooterViewHolder(View itemView) {
-            super(itemView);
-            pb = (ProgressBar) itemView.findViewById(R.id.pb_footer_view);
-            tv = (TextView) itemView.findViewById(R.id.tv_footer_view);
-        }
-    }
-
 }
